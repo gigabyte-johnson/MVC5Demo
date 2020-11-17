1d429d98f782f686cb9f9fdef55ebd43ffeb6b02
gigabyte-johnson <gigabyte.johnson.li@gmail.com>
2020-11-17 21:59:54 +0800

    Add TestController, Person Model & Test/Index View

---
+++ MVC5Demo/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = new List<Person>() {
+                new Person() {Id=1, Name="johnson", Age=26 },
+                new Person() {Id=2, Name="Nadia", Age=25 },
+            };
+            return View(data);
+        }
+    }
+}
