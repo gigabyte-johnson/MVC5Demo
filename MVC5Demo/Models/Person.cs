2df40b420c868f17e9ede212af14422473425008
gigabyte-johnson <gigabyte.johnson.li@gmail.com>
2020-11-18 21:03:15 +0800

    Model 加入[DisplayName]顯示客製名稱

--- MVC5Demo/Models/Person.cs
+++ MVC5Demo/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,12 @@ namespace MVC5Demo.Models
     public class Person
     {
         public int Id { get; set; }
+
+        [DisplayName("姓名")]
         [Required(ErrorMessageResourceName = "Person_Name_Required", ErrorMessageResourceType = typeof(Resource1))] 
         public string Name { get; set; }
+
+        [DisplayName("年齡")]
         [Required(ErrorMessage = "請輸入年紀")]
         [Range(18,99, ErrorMessage = "年紀範圍18~99")]
         public int Age { get; set; }
