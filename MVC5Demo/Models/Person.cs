d42a9fb3bd1766dc212e3493bd9b57987e675c01
GIGABYTE\johnson.li <gigabyte.johnson.li@gmail.com>
2020-11-18 18:08:11 +0800

    加入前端驗證和多國語系

--- MVC5Demo/Models/Person.cs
+++ MVC5Demo/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace MVC5Demo.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessageResourceName = "Person_Name_Required", ErrorMessageResourceType = typeof(Resource1))] 
         public string Name { get; set; }
+        [Required(ErrorMessage = "請輸入年紀")]
+        [Range(18,99, ErrorMessage = "年紀範圍18~99")]
         public int Age { get; set; }
     }
 }
